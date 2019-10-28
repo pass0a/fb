218182af9197b8285154cf8505b3a89eb2f1fc8e
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-10-28 16:45:34 +0800

    updated copy file path

--- modules/copy.js
+++ modules/copy.js
@@ -1,7 +1,7 @@
 var fs = require('fs-extra');
 var path = require('path');
 fs
-	.copy('../output/node_modules/img/', path.resolve(__dirname, 'dist/src'))
+	.copy('../output/node_modules/fb/', path.resolve(__dirname, 'dist/src'))
 	.then(() => {
 		console.log('copy core success!');
 	})
